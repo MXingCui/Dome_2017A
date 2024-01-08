5ff886ee0e670014ac19f22c2a41ea17af844505
Cpri <2248164199@qq.com>
2024-01-08 13:59:47 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/cuimingxing/dome_2017a/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bw.cuimingxing.dome_2017a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
